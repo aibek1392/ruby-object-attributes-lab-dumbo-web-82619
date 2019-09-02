a75721644264bbb0a154770995903531ac31f9c9
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-02 19:35:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   attr_writer :name
-  def initialize(name,breed)
-    @name = name
-    @breed = breed
-  end
+#   def initialize(name,breed)
+#     @name = name
+#     @breed = breed
+#   end
   
-end
+# end
 
 # dog = Dog.new("Rex")
